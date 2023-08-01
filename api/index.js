a7795589615092a794048321e0f232c82fae3703
Claire Lee <lee.claire156@gmail.com>
2023-08-01 11:11:33 -0400

    Establishes MongoDB connection with mongoose

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
 
 app.get('/test', (req, res)=>{
     res.json('test ok');
+});
+
+app.post('/register', (req, res)=>{
+    res.json('register ok');
 });
 
 app.listen(4000);
