3e2f9c20b98d2aff12f8f29380b6bc6c1b69e615
Claire Lee <lee.claire156@gmail.com>
2023-08-01 21:48:45 -0400

    Allows users to register with JWT and cookies

--- api/index.js
+++ api/index.js
@@ -1,17 +1,53 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const jwtSecret = `${process.env.JWT_SECRET}`;
 
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGODB_URL);
 
-app.get('/test', (req, res)=>{
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+}));
+
+app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
-app.post('/register', (req, res)=>{
-    res.json('register ok');
+app.get('/profile', (req, res) => {
+    const token = req.cookies?.token;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+            if (err) throw err;
+            res.json(userData);
+        });
+    } else {
+        res.status(401).json('No token found');
+    }
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const newUser = await User.create({ username, password });
+        jwt.sign({ userId: newUser._id, username }, jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
+                id: newUser._id,
+            });
+        });
+    } catch (err) {
+        if (err) throw err;
+        res.status(500).json('error')
+    }
 });
 
 app.listen(4000);
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import axios from 'axios';
+import { UserContext } from "./UserContext";
 
 export default function Register() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const {setUsername:setLoggedInUsername, setId} = useContext(UserContext)
+    
+    async function register(event) {
+        event.preventDefault();
+        const {data} = await axios.post('/register', { username, password });
+        setLoggedInUsername(username);
+        setId(data.id);
+    }
 
     return (
         <div className="bg-blue-50 h-screen flex items-center">
-            <form className="w-64 mx-auto mb-12">
-                <input value={username} 
-                    onChange={event => setUsername(event.target.value)} 
-                    type="text" 
-                    placeholder="username" 
+            <form className="w-64 mx-auto mb-12" onSubmit={register}>
+                <input value={username}
+                    onChange={event => setUsername(event.target.value)}
+                    type="text"
+                    placeholder="username"
                     className="block w-full rounded-sm p-2 mb-2 border" />
-                <input value={password} 
-                    onChange={event => setPassword(event.target.value)} 
-                    type="password" 
-                    placeholder="password" 
+                <input value={password}
+                    onChange={event => setPassword(event.target.value)}
+                    type="password"
+                    placeholder="password"
                     className="block w-full rounded-sm p-2 mb-2 border" />
                 <button className="bg-blue-500 text-white block w-full rounded-sm p-2">Register</button>
             </form>
